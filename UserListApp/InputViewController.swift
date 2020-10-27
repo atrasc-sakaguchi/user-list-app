091a87252792cbd4b92e9c514c254316d04af7fc
坂口美月 <sakaguchimitsuki@CL000012902.local>
2020-10-27 13:11:53 +0900

    second commit

--- UserListApp/InputViewController.swift
+++ UserListApp/InputViewController.swift
@@ -10,42 +10,23 @@ import UIKit
 
 class InputViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    @IBOutlet weak var YakusyokuLabel: UILabel!
-    @IBOutlet weak var NameLabel: UILabel!
-    @IBOutlet weak var BusyoLabel: UILabel!
-    @IBOutlet weak var TellLabel: UILabel!
-    //役職入力テキストボックス
-    @IBAction func YakusyokuText(_ sender: UITextField) {
-        
-    }
-    //氏名入力テキストボックス
-    @IBAction func NameInputText(_ sender: UITextField) {
-    }
-    //部署入力テキストボックス
-    @IBAction func BusyoInoutText(_ sender: UITextField) {
-    }
-    //連絡先入力テキストボックス
-    @IBAction func TelInputText(_ sender: UITextField) {
-    }
-    //保存ボタン
-    @IBAction func SaveButton(_ sender: UIButton) {
-        
-        
+    @IBOutlet weak var yakusyokuInputText: UITextField!
+    @IBOutlet weak var nameInputText: UITextField!
+    @IBOutlet weak var busyoInputText: UITextField!
+    @IBOutlet weak var tellInputText: UITextField!
+    
+    
+    @IBOutlet weak var yakusyokuLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var busyoLabel: UILabel!
+    @IBOutlet weak var tellLabel: UILabel!
+    
+    @IBAction func saveButton(_ sender: UIButton) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
-    */
-
 }
+
