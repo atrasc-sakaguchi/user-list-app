5fd58a6e6dc2a19d7cd000156823ff8374a7984a
坂口美月 <sakaguchimitsuki@CL000012902.local>
2020-10-27 17:44:45 +0900

    step1 commit

--- UserListApp/InputViewController.swift
+++ UserListApp/InputViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class InputViewController: UIViewController {
 
+    var addUsers:[String] = []
+    
     @IBOutlet weak var yakusyokuInputText: UITextField!
     @IBOutlet weak var nameInputText: UITextField!
     @IBOutlet weak var busyoInputText: UITextField!
     @IBOutlet weak var tellInputText: UITextField!
-    
     
     @IBOutlet weak var yakusyokuLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var busyoLabel: UILabel!
     @IBOutlet weak var tellLabel: UILabel!
     
+    
+    //保存ボタンクリック
     @IBAction func saveButton(_ sender: UIButton) {
+
+        //入力された値を取得
+        let title = yakusyokuInputText.text!    //役職
+        let name = nameInputText.text!      //氏名
+        let department = busyoInputText.text!       //部署
+        let phone = tellInputText.text!     //連絡先
+
+        //追加
+        TableViewController.users += [(title,name,department,phone)]
+        
+        //追加した行データの保存
+        let defaults = UserDefaults.standard
+        
+        
     }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- UserListApp/TableViewController.swift
+++ UserListApp/TableViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
+    
     //テーブルに表示するデータ一覧
-    var users:[(name: String, department: String, Yaku: String, phone: String )] = [
+    static var users:[(name: String, department: String, title: String, phone: String )] = [
     ("山田　太郎","システム開発部","課長代理","090-9999-9999"),
     ("川田　吾郎","営業部","一般","080-8888-8888"),
     ("里田　舞","人事部","一般","070-7777-7777"),
     ("道上　歩","人事部","部長","070-0000-0000"),
     ("海川　泳三","総務部","係長","080-3333-3333"),
     ("林　九郎","システム開発部","課長","080-3333-3333"),
-    ("土屋　草五郎","システム開発部","一般","０090-9900-8877"),
+    ("土屋　草五郎","システム開発部","一般","090-9900-8877"),
     ("竹下　大作","システム開発部","一般","090-4455-1133"),
     ("木田　房枝","運用部","一般","070-2222-5533"),
     ("砂川　黄太郎","運用部","次長","090-1212-3434"),
@@ -26,11 +27,13 @@ class TableViewController: UITableViewController {
     ("水上　泉太郎","総務部","一般","090-0099-7722"),
     ("畑山　耕史","システム開発部","本部長","090-2929-3535"),
     ("浪江　乗太郎","システム開発部","係長代理補佐","080-8787-5454")
-    ]
+    ];
 
+    var users:[(name: String, department: String, title: String, phone: String )] = [];
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
+        super.viewDidLoad();
+        self.users = TableViewController.users;
     }
 
     // MARK: - Table view data source
@@ -44,7 +47,7 @@ class TableViewController: UITableViewController {
     //セクションごとの行数を返す
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
-        return users.count
+        return users.count+1
     }
     
     //セルを作成して返す
@@ -60,23 +63,23 @@ class TableViewController: UITableViewController {
         
         return secondCell
         
-        case 1:
-        //情報表示用のセル設定
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-            
-        //セルの内容を指定
-        cell.textLabel?.text = users[indexPath.row].name
-        cell.detailTextLabel?.text = users[indexPath.row].department
-            
-        return cell
+//        case 1:
+//        //情報表示用のセル設定
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+//
+//        //セルの内容を指定
+//        cell.textLabel?.text = users[indexPath.row].name
+//        cell.detailTextLabel?.text = users[indexPath.row].department
+//
+//        return cell
         
             
         default:
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
         
         //セルの内容を指定
-        cell.textLabel?.text = users[indexPath.row].name
-        cell.detailTextLabel?.text = users[indexPath.row].department
+        cell.textLabel?.text = users[indexPath.row-1].name  //氏名
+        cell.detailTextLabel?.text = users[indexPath.row-1].department  //部署
         
         return cell
         }
@@ -85,26 +88,25 @@ class TableViewController: UITableViewController {
     
     //セル削除
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        //削除
         if editingStyle == .delete{
             users.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
-
-        else{
-            
+        else {
         }
     }
 
-//画面遷移
+    //画面遷移
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let Syosai = segue.destination as? SyosaiViewController else {return}
         guard let indexPath = tableView.indexPathForSelectedRow else {return}
-        
-        Syosai.Yakusyoku = users[indexPath.row].Yaku
-        print(users[indexPath.row].Yaku)
-        Syosai.Name = users[indexPath.row].name
-        Syosai.Busyo = users[indexPath.row].department
-        Syosai.Renraku = users[indexPath.row].phone
+ 
+        //詳細表示画面に情報を表示
+        Syosai.Name = users[indexPath.row-1].name   //氏名
+        Syosai.Busyo = users[indexPath.row-1].department    //部署
+        Syosai.Yakusyoku = users[indexPath.row-1].title    //役職
+        Syosai.Renraku = users[indexPath.row-1].phone    //連絡先
     }
         
         
